c39acd850d436ad66362acee553cc25601c39ea8
Fredde <welandfr@arcada.fi>
2024-04-05 08:35:12 +0000

    add psycopg

--- api/hotel/app.py
+++ api/hotel/app.py
@@ -1,17 +1,36 @@
+import os, psycopg
+from psycopg.rows import dict_row
 from flask import Flask, request
-from flask_cors import CORS 
+from flask_cors import CORS
+from dotenv import load_dotenv 
+
+# pip install psycopg_binary python-dotenv
+
+load_dotenv()
 
 PORT=8802 #freddes port
+
+db_url = os.environ.get("DB_URL")
+print(os.environ.get("FOO"))
+
+conn = psycopg.connect(db_url, autocommit=True, row_factory=dict_row)
 
 app = Flask(__name__)
 CORS(app) # Tillåt cross-origin requests
 
-rooms = [
+roomsTEMP = [
     { 'number': 101, 'type': "single" },
     { 'number': 202, 'type': "double" },
     { 'number': 303, 'type': "suite" }
 ]
 
+@app.route("/test", )
+def dbtest():
+    with conn.cursor() as cur:
+        cur.execute("SELECT * from people")
+        rows = cur.fetchall()
+        return rows
+        
 @app.route("/", )
 def info():
     #return "<h1>Hello, Flask!</h1>"
@@ -22,17 +41,17 @@ def rooms_endoint():
     if request.method == 'POST':
         request_body = request.get_json()
         print(request_body)
-        rooms.append(request_body)
+        roomsTEMP.append(request_body)
         return { 
-            'msg': f"Du har skapat ett nytt rum, id: {len(rooms)-1}!"
+            'msg': f"Du har skapat ett nytt rum, id: {len(roomsTEMP)-1}!"
         }
     else:
-        return rooms
+        return roomsTEMP
 
 @app.route("/rooms/<int:id>", methods=['GET', 'PUT', 'PATCH', 'DELETE'] )
 def one_room_endpoint(id):
         if request.method == 'GET':
-            return rooms[id]
+            return roomsTEMP[id]
         
 
 if __name__ == '__main__':
